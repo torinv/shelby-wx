832b4162e20b482e37787cd63e94ecc4688cb2d2
Torin <tvermettewps@gmail.com>
2021-11-29 00:07:42 -0800

    Finish up flask app; working on time lapse driver

--- app/ShelbyWx.py
+++ app/ShelbyWx.py
@@ -1,13 +1,41 @@
-from flask import Flask, render_template
-from flask_cors import CORS
-from TimeLapseDriver import TimeLapseDriver
+from flask import Flask, render_template, jsonify, request
+from TimeLapseDriver import TimeLapseDriver, TimeLapseUnit
 import json
+from threading import Thread
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+time_lapse_driver = TimeLapseDriver()
+
+@app.route('/', methods=['GET', 'POST'])
+def index_save_time_lapse():
+    if request.form.get('save') == 'Save Time Lapse':
+        time_lapse_driver.save_time_lapse()
+
+    if request.form.get('update') == 'Update':
+        try:
+            int(request.form['frames'])
+        except:
+            pass
+
+        time_lapse_driver.update_params(int(request.form['frames']), int(request.form['units']))
+
     return render_template('index.html')
 
+@app.route('/_update_wx_data', methods=['GET'])
+def update_wx_data():
+    # TODO: requests.get the actual AW API here
+    return json.load(open('./test_data_event.json', 'r'))
+
+@app.route('/_get_latest_time_lapse', methods=['GET'])
+def get_latest_time_lapse():
+    return {}
+
+@app.route('/_get_time_lapse_params', methods=['GET'])
+def get_time_lapse_params():
+    return jsonify(num_frames=time_lapse_driver.num_frames, unit=time_lapse_driver.unit.value)
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    Thread(target=time_lapse_driver.run).start()
+    Thread(target=app.run).start()
--- app/TimeLapseDriver.py
+++ app/TimeLapseDriver.py
@@ -1,12 +1,64 @@
-import asyncio
+import time
+import requests
+import os
+import datetime
+import shutil
+from enum import Enum
+from requests.auth import HTTPDigestAuth
+
+class TimeLapseUnit(Enum):
+    MINUTE = 1
+    HOUR = 2
+    DAY = 3
 
 class TimeLapseDriver(object):
-    @staticmethod
-    async def run():
+    def __init__(self):
+        self.num_frames = 1
+        self.unit = TimeLapseUnit.MINUTE
+
+        self._counter = 0
+        self._seconds_to_wait = 1
+
+    def run(self):
         while(True):
-            await asyncio.sleep(1)
-            print("Hello world")
+            time.sleep(1)
+
+            if self._counter < self._seconds_to_wait:
+                self._counter += 1
+                continue
+
+            self._counter = 0
+            self.take_snapshot()
+
+    def update_params(self, num_frames, unit):
+        # Set new parameters and reset counter
+        self.num_frames = num_frames
+        self.unit = TimeLapseUnit(unit)
+
+        self._counter = 0
+
+        # Calculate seconds between snapshot
+        if self.unit == TimeLapseUnit.MINUTE:
+            self._seconds_to_wait = 60 // self.num_frames
+        elif self.unit == TimeLapseUnit.HOUR:
+            self._seconds_to_wait = 3600 // self.num_frames
+        elif self.unit == TimeLapseUnit.DAY:
+            self._seconds_to_wait = 86400 // self.num_frames
+
+    @staticmethod
+    def take_snapshot():
+        url = 'http://192.168.1.250/cgi-bin/snapshot.cgi'
+        response = requests.get(url, auth=HTTPDigestAuth(os.environ['CAM_USER'], os.environ['CAM_PASSWORD']))
+        breakpoint()
+        if response.status_code == 200:
+            # TODO: this doesn't work; figure out how to save off images
+            with open('../test/' + str(datetime.datetime.now()) + '.jpeg', 'wb') as f:
+                shutil.copyfileobj(response.raw, f)
 
     @staticmethod
     def save_time_lapse():
+        print("it works!!")
+
+    @staticmethod
+    def get_latest_time_lapse():
         pass
